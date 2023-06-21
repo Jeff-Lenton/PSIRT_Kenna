48f3d858093c9a1b0bebe3e050a7efb7fb95ac4e
Jeff-Lenton <jeff@pitchupmusic.com>
2023-06-21 12:03:04 +0100

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,12 @@
+# API Credentials
+
+# Cisco PSIRT
+# Get credentials here: https://developer.cisco.com/docs/psirt/#!authentication
+
+client_id = 'client_id_here'
+client_secret = 'client_secret_here'
+
+# Kenna VI+ API
+# Requires Kenna subscription
+
+kenna_api_token = "kenna_token_here"
