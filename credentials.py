fa39966f9537d5f75503ba92d8049c882f95f3dd
Jeff-Lenton <jeff@pitchupmusic.com>
2023-06-21 15:59:32 +0100

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -3,10 +3,10 @@
 # Cisco PSIRT
 # Get credentials here: https://developer.cisco.com/docs/psirt/#!authentication
 
-client_id = 'client_id_here'
-client_secret = 'client_secret_here'
+client_id = '3494q48f3jfdk8dbvhtagdw5'
+client_secret = 'RsYHQZW2tKtAHzqXB7Qpfdfg'
 
 # Kenna VI+ API
 # Requires Kenna subscription
 
-kenna_api_token = "kenna_token_here"
+kenna_api_token = "CmYzGyiY-M5CThfxinmnrRG3WTCgzDYtAvQNHrHrEUdQGYLYEK7U8azLPLcHSqxX$2a$06$cNhCuykBSkEqkSV14pa8A."
